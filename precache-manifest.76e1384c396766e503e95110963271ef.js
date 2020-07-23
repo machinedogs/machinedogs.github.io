eb92a01e6f752e9a70249f085f715fa49c4df1c9
mslipenchuk267 <mslipenchuk267@gmail.com>
2020-07-23 15:34:34 -0400

    Updates

---
+++ precache-manifest.76e1384c396766e503e95110963271ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da135abc22ecb857eeb2a2073abc2fd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6aed912304745c895281",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ed0b929c1886c9084c90",
+    "url": "/static/js/2.9c8f428c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9c8f428c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aed912304745c895281",
+    "url": "/static/js/main.e235ff14.chunk.js"
+  },
+  {
+    "revision": "07d923f7a861524e1234",
+    "url": "/static/js/runtime-main.481ee7d3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
